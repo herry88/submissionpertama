2d334a87bd3ef42c6e54646a0922162dde0691d2
Herry Prasetyo <herry081288@gmail.com>
2022-06-26 14:58:41 +0700

    adddepedency

--- lib/helper/theme.dart
+++ lib/helper/theme.dart
@@ -1,15 +1,89 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'package:submissionpertama/constanta/color.dart';
 import 'package:submissionpertama/provider/themeprovider.dart';
 
 ThemeData themeLight(BuildContext context) {
-  final _theme = Provider.of<ThemeProvider>(context);
+  final  theme = Provider.of<ThemeProvider>(context);
+
+  Color _primaryLight(ThemeUIKit val) {
+    switch (val) {
+      case ThemeUIKit.foody:
+        return PrimaryColorLight.foody;
+      default:
+        return PrimaryColorLight.coba;
+    }
+  }
 
   return ThemeData(
     brightness: Brightness.light,
     backgroundColor: ColorLight.background,
     cardColor: ColorLight.card,
     disabledColor: ColorLight.disabledButton,
+    errorColor: ColorLight.error,
+    hintColor: ColorLight.fontSubtitle,
+    indicatorColor: _primaryLight(theme.themeUIKit),
+    primaryColor: _primaryLight(theme.themeUIKit),
+    textSelectionTheme: TextSelectionThemeData(
+      cursorColor: _primaryLight(theme.themeUIKit),
+    ),
+    scaffoldBackgroundColor: ColorLight.background,
+    appBarTheme: const AppBarTheme(
+      elevation: 0,
+      centerTitle: true,
+    ),
+    textTheme: GoogleFonts.poppinsTextTheme().copyWith(
+      headline1: GoogleFonts.poppins(
+        color: ColorLight.fontTitle,
+        fontSize: 20,
+        fontWeight: FontWeight.w500,
+      ),
+      headline2: GoogleFonts.poppins(
+        color: ColorLight.fontTitle,
+        fontSize: 18,
+        fontWeight: FontWeight.w500,
+      ),
+      headline3: GoogleFonts.poppins(
+        color: ColorLight.fontTitle,
+        fontSize: 16,
+        fontWeight: FontWeight.w500,
+      ),
+      headline4: GoogleFonts.poppins(
+        color: ColorLight.fontTitle,
+        fontSize: 14,
+        fontWeight: FontWeight.w500,
+      ),
+      headline5: GoogleFonts.poppins(
+        color: ColorLight.fontTitle,
+        fontSize: 12,
+        fontWeight: FontWeight.w500,
+      ),
+      bodyText1: GoogleFonts.poppins(
+        color: ColorLight.fontTitle,
+        fontSize: 16,
+        fontWeight: FontWeight.normal,
+      ),
+      bodyText2: GoogleFonts.poppins(
+        color: ColorLight.fontTitle,
+        fontSize: 14,
+        fontWeight: FontWeight.normal,
+      ),
+      subtitle1: GoogleFonts.poppins(
+        color: ColorLight.fontSubtitle,
+        fontSize: 14,
+        fontWeight: FontWeight.normal,
+      ),
+      subtitle2: GoogleFonts.poppins(
+        color: ColorLight.fontSubtitle,
+        fontSize: 12,
+        fontWeight: FontWeight.normal,
+      ),
+      button: GoogleFonts.poppins(
+        color: Colors.white,
+        fontSize: 14,
+        fontWeight: FontWeight.normal,
+      ),
+    ),
   );
 }
