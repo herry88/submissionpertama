634a9a70c98d6a0814856b69ac466256f4b8aabc
Herry Prasetyo <herry081288@gmail.com>
2022-06-27 13:02:44 +0700

    tambahanAgain

---
+++ lib/screen/onboarding/widget/build_get_started.dart
@@ -0,0 +1,10 @@
+part of '../onboarding_page.dart';
+
+class BuildGetStartedButton extends StatelessWidget {
+  const BuildGetStartedButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(child: Center());
+  }
+}
