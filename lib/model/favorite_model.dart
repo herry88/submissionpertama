7151625bcb3357f289438dd75720e482d035cd37
Herry Prasetyo <herry081288@gmail.com>
2022-06-27 13:24:35 +0700

    foodModelBlm

--- lib/model/favorite_model.dart
+++ lib/model/favorite_model.dart
@@ -1,8 +1,15 @@
-class  FavoriteModel {
-  final int? id; 
-  
+import 'package:submissionpertama/model/food_model.dart';
+
+class FavoriteModel {
+  final int? id;
+  final FoodModel? food;
+  bool isLiked;
 
   FavoriteModel({
     this.id,
+    this.food,
+    this.isLiked = false,
   });
 }
+
+List<FavoriteModel> mockFavoriteList = [];
