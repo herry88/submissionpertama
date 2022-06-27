b402f0f99f69f06c93de71674d7e0afbf525d722
Herry Prasetyo <herry081288@gmail.com>
2022-06-27 12:38:25 +0700

    widget

--- lib/screen/onboarding/onboarding_page.dart
+++ lib/screen/onboarding/onboarding_page.dart
@@ -1,2 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+
+part './screen/onboarding_screen.dart';
+part './widget/buildskipbutton.dart';
---
+++ lib/screen/onboarding/widget/buildskipbutton.dart
@@ -0,0 +1,12 @@
+part of '../onboarding_page.dart';
+
+class BuildSkipButton extends StatelessWidget {
+  const BuildSkipButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      child: InkWell(),
+    );
+  }
+}
